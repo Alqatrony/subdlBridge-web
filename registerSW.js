d45a6011267a6ed43f60b1f821b37b8de424db22
Alqatrony <59061290+Alqatrony@users.noreply.github.com>
2025-05-12 01:08:20 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/subdlBridge-web/sw.js', { scope: '/subdlBridge-web/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
